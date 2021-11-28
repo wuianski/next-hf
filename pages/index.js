0b813231784f7ebe800bf81fd8602bf3cb063c51
wuianski <wuiyeh@gmail.com>
2021-11-29 03:03:04 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -71,48 +71,50 @@ function Index({ summary, contact }) {
           <Image src="/IMGs/logo_w.png" alt="logo" width={185} height={75} />
         </div>
       </Link>*/}
-      <Box sx={{ backgroundColor: "#000000", width: "100vw", height: "100vh" }}>
+      <ReactScrollWheelHandler
+        //upHandler={(e) => console.log("scroll up")}
+        downHandler={() => router.push("/main#events")}
+      >
         <Box
-          sx={{
-            width: { xs: "100vw", md: "50vw" },
-            height: { xs: "100vw", md: "50vw" },
-            position: "absolute",
-            top: { xs: "8vh", md: "0px" },
-            right: { xs: "0px", md: "50px" },
-            zIndex: 0,
-          }}
+          sx={{ backgroundColor: "#000000", width: "100vw", height: "100vh" }}
         >
-          <Image
-            layout="responsive"
-            src="/IMGs/frontPage_bg.png"
-            alt="front page background image"
-            objectFit="cover"
-            objectPosition="center"
-            width={500}
-            height={500}
-          />
-        </Box>
-        {/* gradient black bg */}
-        <Box
-          sx={{
-            display: { xs: "none", md: "block" },
-            width: { xs: "20vw", md: "50vw" },
-            height: "100vh",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            zIndex: 2,
-            backgroundImage:
-              "linear-gradient(90deg, #000000 0%, rgba(0, 0, 0, 1.0) 60%, rgba(0, 0, 0, 0.5) 80%, rgba(0, 0, 0, 0) 100%)",
-          }}
-        />
-        {/* /gradient black bg */}
-
-        <Link href="/main#events">
-          <ReactScrollWheelHandler
-            //upHandler={(e) => console.log("scroll up")}
-            downHandler={() => router.push("/main#events")}
+          <Box
+            sx={{
+              width: { xs: "100vw", md: "50vw" },
+              height: { xs: "100vw", md: "50vw" },
+              position: "absolute",
+              top: { xs: "8vh", md: "0px" },
+              right: { xs: "0px", md: "50px" },
+              zIndex: 0,
+            }}
           >
+            <Image
+              layout="responsive"
+              src="/IMGs/frontPage_bg.png"
+              alt="front page background image"
+              objectFit="cover"
+              objectPosition="center"
+              width={500}
+              height={500}
+            />
+          </Box>
+          {/* gradient black bg */}
+          <Box
+            sx={{
+              display: { xs: "none", md: "block" },
+              width: { xs: "20vw", md: "50vw" },
+              height: "100vh",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              zIndex: 2,
+              backgroundImage:
+                "linear-gradient(90deg, #000000 0%, rgba(0, 0, 0, 1.0) 60%, rgba(0, 0, 0, 0.5) 80%, rgba(0, 0, 0, 0) 100%)",
+            }}
+          />
+          {/* /gradient black bg */}
+
+          <Link href="/main#events">
             <Box
               sx={{
                 position: "absolute",
@@ -129,194 +131,194 @@ function Index({ summary, contact }) {
                 height={55}
               />
             </Box>
-          </ReactScrollWheelHandler>
-        </Link>
+          </Link>
 
-        {/* logo ani */}
-        <Box
-          sx={{
-            zIndex: 8,
-            position: "absolute",
-            bottom: "18vh",
-            left: "18vh",
-            overflow: { xs: "hidden", md: "unset" },
-            display: { xs: "none", md: "block" },
-          }}
-        >
-          <motion.div
-            initial={{ scale: 1.2 }}
-            animate={{ scale: [1.2, 0.8, 0.8, 1] }}
-            exit={{ scale: 1 }}
-            transition={{
-              duration: 17.5,
-              times: [0, 0.05, 0.96, 1],
-              delay: 0.3,
+          {/* logo ani */}
+          <Box
+            sx={{
+              zIndex: 8,
+              position: "absolute",
+              bottom: "18vh",
+              left: "18vh",
+              overflow: { xs: "hidden", md: "unset" },
+              display: { xs: "none", md: "block" },
             }}
           >
-            <Box>
-              <Image
-                src="/IMGs/logo_w.png"
-                alt="logo"
-                width={256}
-                height={103}
-              />
-            </Box>
-          </motion.div>
-        </Box>
-        {/* /logo ani */}
-        {/* logo ani (mobile) */}
-        <Box
-          sx={{
-            zIndex: 8,
-            position: "absolute",
-            bottom: "50vh",
-            left: "10vw",
-            overflow: { xs: "unset", md: "unset" },
-            display: { xs: "block", md: "none" },
-          }}
-        >
-          <motion.div
-            initial={{ scale: 1.2, y: 0 }}
-            animate={{ scale: [1.2, 0.8, 0.8, 1], y: [0, 0, 0, 100] }}
-            exit={{ scale: 1, y: 0 }}
-            transition={{
-              duration: 17.5,
-              times: [0, 0.05, 0.96, 1],
-              delay: 0.3,
+            <motion.div
+              initial={{ scale: 1.2 }}
+              animate={{ scale: [1.2, 0.8, 0.8, 1] }}
+              exit={{ scale: 1 }}
+              transition={{
+                duration: 17.5,
+                times: [0, 0.05, 0.96, 1],
+                delay: 0.3,
+              }}
+            >
+              <Box>
+                <Image
+                  src="/IMGs/logo_w.png"
+                  alt="logo"
+                  width={256}
+                  height={103}
+                />
+              </Box>
+            </motion.div>
+          </Box>
+          {/* /logo ani */}
+          {/* logo ani (mobile) */}
+          <Box
+            sx={{
+              zIndex: 8,
+              position: "absolute",
+              bottom: "50vh",
+              left: "10vw",
+              overflow: { xs: "unset", md: "unset" },
+              display: { xs: "block", md: "none" },
             }}
           >
-            <Box>
-              <Image
-                src="/IMGs/logo_w.png"
-                alt="logo"
-                width={256}
-                height={103}
-              />
-            </Box>
-          </motion.div>
-        </Box>
-        {/* /logo ani (mobile) */}
-        <Box
-          sx={{
-            position: "absolute",
-            bottom: "18vh",
-            right: 0,
-            overflow: { xs: "hidden", md: "unset" },
-            width: { xs: "100vw", md: "50vw" },
-          }}
-        >
-          <motion.div
-            initial={{ x: "100vw" }}
-            animate={{ x: ["100vw", "10vw", "-100vw"] }}
-            exit={{ x: "-100vw" }}
-            transition={{ duration: 8, times: [0, 0.8, 1], delay: 0.3 }}
+            <motion.div
+              initial={{ scale: 1.2, y: 0 }}
+              animate={{ scale: [1.2, 0.8, 0.8, 1], y: [0, 0, 0, 100] }}
+              exit={{ scale: 1, y: 0 }}
+              transition={{
+                duration: 17.5,
+                times: [0, 0.05, 0.96, 1],
+                delay: 0.3,
+              }}
+            >
+              <Box>
+                <Image
+                  src="/IMGs/logo_w.png"
+                  alt="logo"
+                  width={256}
+                  height={103}
+                />
+              </Box>
+            </motion.div>
+          </Box>
+          {/* /logo ani (mobile) */}
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "18vh",
+              right: 0,
+              overflow: { xs: "hidden", md: "unset" },
+              width: { xs: "100vw", md: "50vw" },
+            }}
           >
-            <Box>
-              <Box sx={{ color: "#fff" }}>
-                <Box component="span">{p1[0]}</Box>
-                <Box component="span" className="textAniNumber">
-                  {p1[1]}
+            <motion.div
+              initial={{ x: "100vw" }}
+              animate={{ x: ["100vw", "10vw", "-100vw"] }}
+              exit={{ x: "-100vw" }}
+              transition={{ duration: 8, times: [0, 0.8, 1], delay: 0.3 }}
+            >
+              <Box>
+                <Box sx={{ color: "#fff" }}>
+                  <Box component="span">{p1[0]}</Box>
+                  <Box component="span" className="textAniNumber">
+                    {p1[1]}
+                  </Box>
+                  <Box component="span">{p1[2]}</Box>
                 </Box>
-                <Box component="span">{p1[2]}</Box>
-              </Box>
-              <Box sx={{ color: "#fff" }}>
-                <Box component="span">{p2[0]}</Box>
-                <Box component="span" className="textAniNumber">
-                  {p2[1]}
+                <Box sx={{ color: "#fff" }}>
+                  <Box component="span">{p2[0]}</Box>
+                  <Box component="span" className="textAniNumber">
+                    {p2[1]}
+                  </Box>
+                  <Box component="span">{p2[2]}</Box>
                 </Box>
-                <Box component="span">{p2[2]}</Box>
-              </Box>
-              <Box sx={{ color: "#fff" }}>
-                <Box component="span">{p3[0]}</Box>
-                <Box component="span" className="textAniNumber">
-                  {p3[1]}
+                <Box sx={{ color: "#fff" }}>
+                  <Box component="span">{p3[0]}</Box>
+                  <Box component="span" className="textAniNumber">
+                    {p3[1]}
+                  </Box>
+                  <Box component="span">{p3[2]}</Box>
                 </Box>
-                <Box component="span">{p3[2]}</Box>
               </Box>
-            </Box>
-          </motion.div>
-        </Box>
-        <Box
-          sx={{
-            position: "absolute",
-            bottom: "18vh",
-            right: 0,
-            overflow: { xs: "hidden", md: "unset" },
-            width: { xs: "100vw", md: "50vw" },
-          }}
-        >
-          <motion.div
-            initial={{ x: "100vw" }}
-            animate={{ x: ["100vw", "10vw", "-100vw"] }}
-            exit={{ x: "-100vw" }}
-            transition={{ duration: 8, times: [0, 0.8, 1], delay: 5 }}
+            </motion.div>
+          </Box>
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "18vh",
+              right: 0,
+              overflow: { xs: "hidden", md: "unset" },
+              width: { xs: "100vw", md: "50vw" },
+            }}
           >
-            <Box>
-              <Box sx={{ color: "#fff" }}>
-                <Box component="span">{p4[0]}</Box>
-                <Box component="span" className="textAniNumber">
-                  {p4[1]}
+            <motion.div
+              initial={{ x: "100vw" }}
+              animate={{ x: ["100vw", "10vw", "-100vw"] }}
+              exit={{ x: "-100vw" }}
+              transition={{ duration: 8, times: [0, 0.8, 1], delay: 5 }}
+            >
+              <Box>
+                <Box sx={{ color: "#fff" }}>
+                  <Box component="span">{p4[0]}</Box>
+                  <Box component="span" className="textAniNumber">
+                    {p4[1]}
+                  </Box>
+                  <Box component="span">{p4[2]}</Box>
                 </Box>
-                <Box component="span">{p4[2]}</Box>
-              </Box>
-              <Box sx={{ color: "#fff" }}>
-                <Box component="span">{p5[0]}</Box>
-                <Box component="span" className="textAniNumber">
-                  {p5[1]}
+                <Box sx={{ color: "#fff" }}>
+                  <Box component="span">{p5[0]}</Box>
+                  <Box component="span" className="textAniNumber">
+                    {p5[1]}
+                  </Box>
+                  <Box component="span">{p5[2]}</Box>
                 </Box>
-                <Box component="span">{p5[2]}</Box>
               </Box>
-            </Box>
-          </motion.div>
-        </Box>
-        <Box
-          sx={{
-            position: "absolute",
-            bottom: "18vh",
-            right: 0,
-            overflow: { xs: "hidden", md: "unset" },
-            width: { xs: "100vw", md: "50vw" },
-          }}
-        >
-          <motion.div
-            initial={{ x: "100vw" }}
-            animate={{ x: ["100vw", "10vw", "-100vw"] }}
-            exit={{ x: "-100vw" }}
-            transition={{ duration: 8, times: [0, 0.8, 1], delay: 10 }}
+            </motion.div>
+          </Box>
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "18vh",
+              right: 0,
+              overflow: { xs: "hidden", md: "unset" },
+              width: { xs: "100vw", md: "50vw" },
+            }}
           >
-            <Box>
-              <Box sx={{ color: "#fff" }}>
-                <Box component="span">{p6[0]}</Box>
-                <Box component="span" className="textAniNumber">
-                  {p6[1]}
+            <motion.div
+              initial={{ x: "100vw" }}
+              animate={{ x: ["100vw", "10vw", "-100vw"] }}
+              exit={{ x: "-100vw" }}
+              transition={{ duration: 8, times: [0, 0.8, 1], delay: 10 }}
+            >
+              <Box>
+                <Box sx={{ color: "#fff" }}>
+                  <Box component="span">{p6[0]}</Box>
+                  <Box component="span" className="textAniNumber">
+                    {p6[1]}
+                  </Box>
+                  <Box component="span">{p6[2]}</Box>
                 </Box>
-                <Box component="span">{p6[2]}</Box>
-              </Box>
-              <Box sx={{ color: "#fff" }}>
-                <Box component="span">{p7[0]}</Box>
-                <Box component="span" className="textAniNumber">
-                  {p7[1]}
+                <Box sx={{ color: "#fff" }}>
+                  <Box component="span">{p7[0]}</Box>
+                  <Box component="span" className="textAniNumber">
+                    {p7[1]}
+                  </Box>
+                  <Box component="span">{p7[2]}</Box>
                 </Box>
-                <Box component="span">{p7[2]}</Box>
-              </Box>
-              <Box sx={{ color: "#fff" }}>
-                <Box component="span">{p8[0]}</Box>
-                <Box component="span" className="textAniNumber">
-                  {p8[1]}
+                <Box sx={{ color: "#fff" }}>
+                  <Box component="span">{p8[0]}</Box>
+                  <Box component="span" className="textAniNumber">
+                    {p8[1]}
+                  </Box>
+                  <Box component="span">{p8[2]}</Box>
                 </Box>
-                <Box component="span">{p8[2]}</Box>
-              </Box>
-              <Box sx={{ color: "#fff" }}>
-                <Box component="span">{p9[0]}</Box>
-                <Box component="span" className="textAniNumber">
-                  {p9[1]}
+                <Box sx={{ color: "#fff" }}>
+                  <Box component="span">{p9[0]}</Box>
+                  <Box component="span" className="textAniNumber">
+                    {p9[1]}
+                  </Box>
+                  <Box component="span">{p9[2]}</Box>
                 </Box>
-                <Box component="span">{p9[2]}</Box>
               </Box>
-            </Box>
-          </motion.div>
+            </motion.div>
+          </Box>
         </Box>
-      </Box>
+      </ReactScrollWheelHandler>
     </>
   );
 }

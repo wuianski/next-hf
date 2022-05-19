1c09ac628d532bc0af77820133be8454891a81d9
wuianski <wuiyeh@gmail.com>
2022-05-20 00:47:38 +0800

    v3

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ function Index({ summary, contact, projects, events, news }) {
       />
       <Nav contact={contact} projects={projects} />
 
-      <Box pt={{ xs: 15, md: 23 }} mb={"-7px"} pb={10}>
+      <Box pt={{ xs: 15, md: 23 }} mb={"-7px"} pb={30}>
         <Stack direction={{ xs: "column", md: "row" }} spacing={4}>
           <Item>
             <Box
